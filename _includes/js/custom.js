50f84f8fa261eca3239e8f01023f1f1237a7f068
Tyler Magee Shields <115214762+TylerMS887@users.noreply.github.com>
2023-01-03 13:48:39 +0000

    Update custom.js

--- _includes/js/custom.js
+++ _includes/js/custom.js
@@ -1,3 +1,10 @@
+/*************************************************/
+/*                   WARNING!                    */
+/*     This is the custom JS for FreeNT docs.    */
+/*   If you are not a JavaScript developer, you  */
+/*        should not touch this file.            */
+/*************************************************/
+
 window.matchMedia('(prefers-color-scheme: dark)')
       .addEventListener('change', event => {
   if (event.matches) {
