b9f83b1c69b24b36a936d51b13d05af7600e4ab6
Tyler Magee Shields <115214762+TylerMS887@users.noreply.github.com>
2023-01-06 16:39:56 +0000

    Update custom.js

--- _includes/js/custom.js
+++ _includes/js/custom.js
@@ -5,15 +5,7 @@
 /*        should not touch this file.            */
 /*************************************************/
 
-window.matchMedia('(prefers-color-scheme: dark)')
-      .addEventListener('change', event => {
-  if (event.matches) {
-      jtd.setTheme('dark');
-  } else {
-      jtd.setTheme('light');
-  }
-});
-
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    jtd.setTheme('dark');
+  // dark mode
+  jtd.setTheme("dark");
 }
